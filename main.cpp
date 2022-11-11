966ff39f867f6a9866846d14e95d98808fa44be8
Boerilorenzo03 <117928573+Boerilorenzo03@users.noreply.github.com>
2022-11-11 01:06:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+cout << "inserire numeri reali a,b,c" << endl;
+int a;
+int b;
+int c;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
+cout << "areatriangolo:" << endl;
+cout << (a * b) / 2 << endl;
+cout << "areaquadrato:" << endl;
+cout << a * a << endl;
+cout << "arearettangolo:" << endl;
+cout << a * b << endl;
+cout << "areatrapezio:" << endl;
+cout << ((a + a ) * c) / 2 << endl;
+
 
 
 }
+
